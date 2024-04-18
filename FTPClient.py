af9c4375d701328bfb8dab539a7dc0594cb1849f
ShawnC24 <69652935+ShawnC24@users.noreply.github.com>
2024-04-17 21:39:10 -0400

    modified FTPClient.py to run together

--- FTPClient.py
+++ FTPClient.py
@@ -1,33 +1,63 @@
 import socket
+import os
 
-def connect_to_server(server_host, server_port):
+def connect_server(server_address, server_port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_host, server_port))
+    client_socket.connect((server_address, server_port))
     return client_socket
 
 def send_command(client_socket, command):
     client_socket.send(command.encode())
-    return client_socket.recv(1024).decode()
+
+def receive_response(client_socket):
+    response = client_socket.recv(1024).decode()
+    return response
+
+def list_files(client_socket):
+    send_command(client_socket, "LIST")
+    files_list = receive_response(client_socket)
+    print("Files at server:", files_list)
+
+def retrieve_file(client_socket, filename):
+    send_command(client_socket, f"RETRIEVE {filename}")
+    file_data = client_socket.recv(1024)
+    with open(filename, "wb") as file:
+        file.write(file_data)
+    print(f"File {filename} retrieved successfully.")
+
+def store_file(client_socket, filename):
+    if not os.path.exists(filename):
+        print(f"File {filename} does not exist.")
+        return
+    send_command(client_socket, f"STORE {filename}")
+    with open(filename, "rb") as file:
+        file_data = file.read()
+        client_socket.send(file_data)
+    print(f"File {filename} stored successfully.")
+
+def disconnect(client_socket):
+    send_command(client_socket, "QUIT")
+    client_socket.close()
 
 def main():
-    server_host = input("Enter server hostname/IP address: ")
-    server_port = int(input("Enter server port number: "))
-
-    client_socket = connect_to_server(server_host, server_port)
-
+    server_address = input("Enter server address: ")
+    server_port = int(input("Enter server port: "))
+    client_socket = connect_server(server_address, server_port)
     while True:
-        command = input("FTP> ").strip()
-        if command.startswith("CONNECT"):
-            _, host, port = command.split()
-            client_socket = connect_to_server(host, int(port))
-        else:
-            response = send_command(client_socket, command)
-            print(response)
-
-        if command == "QUIT":
+        command = input("Enter command (LIST, RETRIEVE <filename>, STORE <filename>, QUIT): ")
+        if command.upper() == "LIST":
+            list_files(client_socket)
+        elif command.upper().startswith("RETRIEVE"):
+            filename = command.split()[1]
+            retrieve_file(client_socket, filename)
+        elif command.upper().startswith("STORE"):
+            filename = command.split()[1]
+            store_file(client_socket, filename)
+        elif command.upper() == "QUIT":
+            disconnect(client_socket)
             break
-
-    client_socket.close()
+        else:
+            print("Invalid command. Try again.")
 
 if __name__ == "__main__":
     main()
--- FTPServer.py
+++ FTPServer.py
@@ -1,58 +1,30 @@
 import socket
 import os
 
-SERVER_ROOT = "./server_files"
-
-def list_files():
-    files = os.listdir(SERVER_ROOT)
-    return "\n".join(files)
-
-def retrieve_file(filename):
-    try:
-        with open(os.path.join(SERVER_ROOT, filename), 'rb') as file:
-            return file.read()
-    except FileNotFoundError:
-        return b"File not found."
-
-def store_file(filename, data):
-    with open(os.path.join(SERVER_ROOT, filename), 'wb') as file:
-        file.write(data)
-    return "File stored successfully."
-
-def handle_client_connection(client_socket):
-    while True:
-        data = client_socket.recv(1024).decode()
-        if not data:
-            break
-
-        command = data.split()
-        if command[0] == "LIST":
-            client_socket.send(list_files().encode())
-        elif command[0] == "RETRIEVE":
-            filename = command[1]
-            client_socket.send(retrieve_file(filename))
-        elif command[0] == "STORE":
-            filename = command[1]
-            data = client_socket.recv(1024)
-            response = store_file(filename, data)
-            client_socket.send(response.encode())
-        elif command[0] == "QUIT":
-            break
-
-    client_socket.close()
-
-def start_server(host, port):
+def start_server(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
-    print("FTP Server listening on {}:{}".format(host, port))
+    server_socket.bind(("localhost", port))
+    server_socket.listen(1)
+    print("FTP server started.")
+    return server_socket
 
+def handle_client(client_socket):
     while True:
-        client_socket, addr = server_socket.accept()
-        print("Connected to {}".format(addr))
-        handle_client_connection(client_socket)
-
-    server_socket.close()
-
-if __name__ == "__main__":
-    start_server("127.0.0.1", 8888)
+        command = client_socket.recv(1024).decode()
+        if not command:
+            break
+        if command.upper() == "QUIT":
+            break
+        elif command.upper() == "LIST":
+            files_list = "\n".join(os.listdir())
+            client_socket.send(files_list.encode())
+        elif command.upper().startswith("RETRIEVE"):
+            filename = command.split()[1]
+            if os.path.exists(filename):
+                with open(filename, "rb") as file:
+                    file_data = file.read()
+                    client_socket.send(file_data)
+            else:
+                client_socket.send("File not found.".encode())
+        elif command.upper().startswith("STORE"):
+            filename 
